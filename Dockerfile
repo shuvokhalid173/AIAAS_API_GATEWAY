137d089da56b6b8fbb26d692305a904a73e4831a
shuvokhalid173 <khalidbinarif@outlook.com>
2026-04-01 01:39:44 +0600

    test

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,13 @@ FROM node:22-alpine
 WORKDIR /app
 
 COPY package*.json ./
-
-RUN npm install --production
+RUN npm ci --only=production
 
 COPY . .
 
-EXPOSE 5555
+EXPOSE ${PORT}
 
 CMD ["node", "src/server.js"]
+
+# docker run -d -p 5555:5555 --name aiaas_api_gateway_service shuvo173/khalid_bin_arif_aiaas-api-gateway-service1.0.0
+# docker run -d -p 5555:5555 --name aiaas_api_gateway_service -e PORT=5555 shuvo173/khalid_bin_arif_aiaas-api-gateway-service1.0.0
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# cd ~/apps/AIAAS_API_GATEWAY
-echo "User: $(whoami)"
-echo "PM2 list:"
-/usr/bin/pm2 list
-git pull origin main
-npm install
+# # cd ~/apps/AIAAS_API_GATEWAY
+# echo "User: $(whoami)"
+# echo "PM2 list:"
+# /usr/bin/pm2 list
+# git pull origin main
+# npm install
 
-/usr/bin/pm2 reload api-gateway
+# /usr/bin/pm2 reload api-gateway
+
+
+cd ~/apps/AIAAS_API_GATEWAY
+
+echo "Pulling latest code..."
+git pull origin main
+
+echo "Pulling latest Docker image..."
+docker pull shuvo173/khalid_bin_arif_aiaas-api-gateway-service1.0.0
+
+echo "Restarting containers..."
+docker-compose up -d
+
+echo "Cleaning unused images..."
+docker image prune -f
+
+echo "Deployment done 🚀"
