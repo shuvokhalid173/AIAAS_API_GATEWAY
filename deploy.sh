6dad63465d5bb28b1b3f1703cc7dcf5198544fc5
shuvokhalid173 <khalidbinarif@outlook.com>
2026-03-31 18:15:41 +0600

    test

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+# #!/bin/bash
+# echo "Hello"; 
+# export NVM_DIR="$HOME/.nvm"
+# source ~/.bashrc
 
-export NVM_DIR="$HOME/.nvm"
-source ~/.bashrc
+# cd ~/apps/AIAAS_API_GATEWAY
 
-cd ~/apps/AIAAS_API_GATEWAY
+# git pull origin main
+# npm install
 
-git pull origin main
-npm install
+# /usr/bin/pm2 reload api-gateway
 
-/usr/bin/pm2 reload api-gateway
+echo "User: $(whoami)"
+echo "PM2 list:"
+/usr/bin/pm2 list
