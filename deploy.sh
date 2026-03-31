12f6422bfc4e70a24fb5ab833a358e9e99d41d82
shuvokhalid173 <khalidbinarif@outlook.com>
2026-03-31 18:05:01 +0600

    test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+echo "User: $(whoami)"
 
 cd ~/apps/AIAAS_API_GATEWAY
 git pull origin main
 npm install
-pm2 reload api-gateway
+
+echo "PM2 list:"
+/usr/bin/pm2 list
+/usr/bin/pm2 reload api-gateway
 
 # pm2 start src/server.js --name "api-gateway" -i max
